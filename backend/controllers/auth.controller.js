db91332099e8e4de0f4e21e4b7edc0b2edb9f491
Melos <melos.simeneh@gmail.com>
2025-04-24 10:23:50 +0300

    error handling way refactor

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,24 +1,26 @@
-import mongoose from "mongoose";
+import { AppError } from "../lib/appError.js";
+import { catchAsync } from "../lib/catchAsync.js";
 import User from "../models/user.model.js";
 
-export const signup = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body || {};
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Email already exists" });
-    }
-    const user = await User.create({ name, email, password });
+export const signup = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body || {};
 
-    res
-      .status(200)
-      .json({ success: true, message: "User created successfully", user });
-  } catch (error) {
-    next(error);
+  const userExists = await User.findOne({
+    email: email.toLowerCase().trim(),
+  });
+
+  if (userExists) {
+    throw new AppError("Email already exists", 400);
   }
-};
+
+  const user = await User.create({ name, email, password });
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    user: { ...user.toJSON(), password: undefined },
+  });
+});
 
 export const login = async (req, res) => {
   res.status(200).json({ success: true, message: "login" });
