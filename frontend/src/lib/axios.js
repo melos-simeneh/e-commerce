a52eec243bcbc5b56da53785b53d0f1b5e607435
Melos <melos.simeneh@gmail.com>
2025-04-26 11:03:45 +0300

    frontend: axios update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
+const isDev = import.meta.env.MODE === "development";
+console.log(import.meta.mode);
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: isDev ? "http://localhost:5000/api" : "/api",
   withCredentials: true, // send cookies to the server
 });
-
 export default axiosInstance;
